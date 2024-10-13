acba1c335890b2515b0bd315ada1411c2fb133ee
Sam <samuelkebbell@gmail.com>
2024-10-13 13:14:03 +0100

    Updates

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function reloadPage() {
   window.location.reload();
 }
 
+
+const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close");
+const mobileMenu = document.querySelector(".navbar__mobile");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("open");
+});
